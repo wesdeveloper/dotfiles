e10b0646e975da72cdb9f1581edb379b543f9f33
wesdeveloper <wesley.lopes@willbank.com.br>
2021-09-27 22:07:58 -0300

    chore: update plugins settings

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -62,13 +62,58 @@ require("lsp-colors").setup({
 })
 
 
+local g = vim.g
 local dap_install = require("dap-install")
 
 dap_install.setup({
-	installation_path = "/tmp/test_dap_install/",
-	verbosely_call_debuggers = false,
+	installation_path = "/tmp/test_dap_install/"
 })
 
+-- dap_install.config("jsnode_dbg", {})
 
-dap_install.config("jsnode_dbg", {})
--- dap_install.config("", {})
+-- mfussenegger/nvim-dap
+local dap = require('dap')
+dap.adapters.node2 = {
+  type = 'executable',
+  command = 'node',
+  args = {os.getenv('HOME') .. '/codebase/vscode-node-debug2/out/src/nodeDebug.js'},
+}
+vim.fn.sign_define('DapBreakpoint', {text='🟥', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpointRejected', {text='🟦', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapStopped', {text='⭐️', texthl='', linehl='', numhl=''})
+
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+map('n', '<leader>dh', ':lua require"dap".toggle_breakpoint()<CR>')
+map('n', '<leader>dH', ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+map('n', '<c-k>', ':lua require"dap".step_out()<CR>')
+map('n', '<c-l>', ':lua require"dap".step_into()<CR>')
+map('n', '<c-j>', ':lua require"dap".step_over()<CR>')
+map('n', '<c-h>', ':lua require"dap".continue()<CR>')
+map('n', '<leader>dk', ':lua require"dap".up()<CR>')
+map('n', '<leader>dj', ':lua require"dap".down()<CR>')
+map('n', '<leader>dc', ':lua require"dap".disconnect({ terminateDebuggee = true });require"dap".close()<CR>')
+map('n', '<leader>dr', ':lua require"dap".repl.toggle({}, "vsplit")<CR><C-w>l')
+map('n', '<leader>di', ':lua require"dap.ui.variables".hover()<CR>')
+map('n', '<leader>di', ':lua require"dap.ui.variables".visual_hover()<CR>')
+map('n', '<leader>d?', ':lua require"dap.ui.variables".scopes()<CR>')
+map('n', '<leader>de', ':lua require"dap".set_exception_breakpoints({"all"})<CR>')
+map('n', '<leader>da', ':lua require"debugHelper".attach()<CR>')
+map('n', '<leader>dA', ':lua require"debugHelper".attachToRemote()<CR>')
+map('n', '<leader>di', ':lua require"dap.ui.widgets".hover()<CR>')
+map('n', '<leader>d?', ':lua local widgets=require"dap.ui.widgets";widgets.centered_float(widgets.scopes)<CR>')
+map('n', '<leader>uid', ':lua require"dapui".toggle()<CR>')
+
+-- nvim-telescope/telescope-dap.nvim
+require('telescope').load_extension('dap')
+map('n', '<leader>ds', ':Telescope dap frames<CR>')
+-- map('n', '<leader>dc', ':Telescope dap commands<CR>')
+map('n', '<leader>db', ':Telescope dap list_breakpoints<CR>')
+
+-- theHamsta/nvim-dap-virtual-text and mfussenegger/nvim-dap
+g.dap_virtual_text = true
+
