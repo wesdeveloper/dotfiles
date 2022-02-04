9506243daccda27b96be83ac57e4bdc7bf6ba08f
wesdeveloper <wesley.lopes@willbank.com.br>
2022-02-03 23:44:27 -0300

    chore(neovim): update lua configs

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -1,3 +1,7 @@
 -- Move selected line / block of text in visual mode
 vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv-gv', {noremap = true, silent = true})
+
+-- jump between methods
+vim.api.nvim_set_keymap('n', '<Space>jbnm', ']m', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Space>jbpm', '[m', { noremap = true, silent = true })
